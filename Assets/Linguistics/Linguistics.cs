0a17cc9876ac846b949540c8fa307fd7c5acba46
NickSchade <NickSchade@users.noreply.github.com>
2019-01-21 15:08:47 -0800

    Added Basic Linguistics

---
+++ Assets/Linguistics/Linguistics.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SamizdatEngine.Linguistics
+{
+    public class Linguistics
+    {
+        public static List<string> AllConsonants()
+        {
+            return new List<string>
+        {"B","C","D","F","G","H","J","K","L","M","N","P","R","S","T","SH","CH","TH"};
+        }
+        public static List<string> AllVowels()
+        {
+            return new List<string>
+        {"A","AA","E","EE","I","II","O","OO","U","UU"};
+        }
+        public static List<string> SelectSubset(List<string> list, int n)
+        {
+            List<string> subset = new List<string>();
+            int len = list.Count;
+            for (int i = 0; i < len; i++)
+            {
+                float r = Random.Range(0.0f, 1.0f);
+                if (r < n / (len - i))
+                {
+                    subset.Add(list[i]);
+                    if (subset.Count == n)
+                    {
+                        break;
+                    }
+                }
+            }
+            return subset;
+        }
+        public static List<string> GenerateWordsBasic(int nWords)
+        {
+            Debug.Log("Generating " + nWords + " words");
+            int nMorphemes = 50;
+            List<string> morphemes = GenerateMorphemes(nMorphemes);
+            List<string> words = new List<string>();
+            for (int i = 0; i < nWords; i++)
+            {
+                string newWord = morphemes[Random.Range(0, morphemes.Count)] + morphemes[Random.Range(0, morphemes.Count)];
+                words.Add(newWord);
+            }
+            return words;
+        }
+        public static List<string> GenerateMorphemes(int nMorphemes)
+        {
+            int nVowelSounds = 5;
+            int nConsonantSounds = 10;
+            float pConsonantStart = 0.5f;
+            float pConsonantEnd = 0.5f;
+            List<string> vowels = SelectSubset(AllVowels(), nVowelSounds);
+            List<string> consonants = SelectSubset(AllConsonants(), nConsonantSounds);
+            List<string> morphemes = new List<string>();
+            for (int i = 0; i < nMorphemes; i++)
+            {
+                morphemes.Add(GenerateMorpheme(consonants, vowels, pConsonantStart, pConsonantEnd));
+            }
+            return morphemes;
+        }
+        public static string GenerateMorpheme(List<string> consonants, List<string> vowels, float pConsonantStart, float pConsonantEnd)
+        {
+            string morpheme = "";
+            float rStart = Random.Range(0.0f, 0.1f);
+            float rEnd = Random.Range(0.0f, 0.1f);
+            if (rStart < pConsonantStart)
+            {
+                morpheme += consonants[Random.Range(0, consonants.Count)];
+            }
+            morpheme += vowels[Random.Range(0, vowels.Count)];
+            if (rEnd < pConsonantEnd)
+            {
+                morpheme += consonants[Random.Range(0, consonants.Count)];
+            }
+            return morpheme;
+        }
+    }
+}
---
+++ Assets/Linguistics/LinguisticsTester.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SamizdatEngine.Linguistics;
+
+public class LinguisticsTester : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Starting");
+        List<string> words = Linguistics.GenerateWordsBasic(20);
+        foreach(string s in words)
+        {
+            Debug.Log(char.ToUpper(s[0])+s.Substring(1).ToLower());
+        }
+        Debug.Log("Finished with "+ words.Count + " words");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
